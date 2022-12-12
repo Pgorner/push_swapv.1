24247b6cebed93a1ae07d7b3cf01b9805b843e7f
pgorner <pgorner@student.42heilbronn.de>
2022-12-12 14:27:24 +0100

    final but too slow

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/28 15:29:25 by pgorner           #+#    #+#             */
-/*   Updated: 2022/12/12 13:19:43 by pgorner          ###   ########.fr       */
+/*   Updated: 2022/12/12 14:27:10 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ typedef struct stacks
 {
 	char	**a;
 	char	**b;
-	char	*ret;
+	char	*str;
 	char	**tmp;
 	int		speed;
 	int		prevspeed;
